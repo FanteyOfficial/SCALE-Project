ec1fcc13158afa24aace4a4cb27d1301e32789c3
ZiniFili <101247036+Zifili@users.noreply.github.com>
2023-11-22 17:35:46 +0100

    basic ai

--- Scripts/er_pesce.gd
+++ Scripts/er_pesce.gd
@@ -1,19 +1,22 @@
 extends CharacterBody2D
 
-@export var direction = Vector2.LEFT
-@export var speed = 30.0
 
+@export var speed = 100.0
+@export var jump = -400.0
+@onready var player = $"../Player"
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func set_player(node):
+	player = node
 
 func _physics_process(delta):
+	var direction : Vector2 = player.get_position()
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * delta 
+	else :
+		velocity.y = jump
 	if direction:
-		velocity.x = direction.x * speed
+		velocity.x = position.direction_to(direction).x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	move_and_slide()
