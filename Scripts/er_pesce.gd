bdc17745d51509f0c2d24ddefee87b52e7db1831
ZiniFili <101247036+Zifili@users.noreply.github.com>
2023-11-25 22:56:10 +0100

    fish fixed
    note to self, use state machines for next bosses

--- Scripts/er_pesce.gd
+++ Scripts/er_pesce.gd
@@ -5,12 +5,19 @@ extends CharacterBody2D
 @export var jump = -400.0
 @export var health = 300 
 @export var player : CharacterBody2D
+var aoe_duration : Timer
+var attack_timer : Timer
 var phase_1 = true
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func initialize(attack,aoe):
+	attack_timer = attack 
+	aoe_duration = aoe
 
 func _physics_process(delta):
 	if health <= 0:
 		phase_1 = false
+		health = 300
 	if phase_1:
 		var direction = player.get_position()
 		if not is_on_floor():
@@ -21,6 +28,9 @@ func _physics_process(delta):
 			velocity.x = position.direction_to(direction).x * speed
 		else:
 			velocity.x = move_toward(velocity.x, 0, speed)
+	else :
+		attack_timer.start(5)
+		set_physics_process(false)
 	move_and_slide()
 
 func get_phase():
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -6,13 +6,15 @@ var toothbrush_attack = preload("res://Scenes/toothbrush.tscn")
 @onready var area_attack = $"area_attack"
 @onready var aoe_dur = $"aoe_duration"
 
+func _ready():
+	pesce.initialize(area_attack,aoe_dur)
 
 func _on_area_attack_timeout(): #NB LE SPIKE SONO UNFAIR, PLS FIX
 	area_attack.start(5)
 	var tooth_spawn = get_node("SpawnPath/SpawnLocation")
 	for i in range(10):
 		var tooth = toothbrush_attack.instantiate()
-		tooth_spawn.progress = player.position.x-20 + 100*i
+		tooth_spawn.progress = player.position.x + 100*i
 		tooth.init(tooth_spawn.position)
 		add_child(tooth)
 	aoe_dur.start(1)
